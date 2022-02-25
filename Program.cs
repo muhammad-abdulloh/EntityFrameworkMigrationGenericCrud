404f770acbcb54316b50038a9acee33df89fe080
Clever Coder <92303840+clevercoderr@users.noreply.github.com>
2022-02-25 20:36:11 +0500

    Get Student By Id

--- Program.cs
+++ Program.cs
@@ -1,25 +1,21 @@
 ﻿using System;
+using System.Threading.Tasks;
 using static System.Console;
 
 namespace TheroToHeroCRUDInVSCode
 {
     class Program
     {
-        static NajotTalimDbContext _najotTalimDbContext = new NajotTalimDbContext();
-        static void Main(string[] args)
+        // static NajotTalimDbContext _najotTalimDbContext = new NajotTalimDbContext();
+        #pragma warning disable CS0169 // The field 'Program.studentRepository' is never used
+        static async Task Main(string[] args)
         {
-            var students = _najotTalimDbContext.Students;
+            ISudentRepository sudentRepository = new StudentRepository();
+            var student = await sudentRepository.Get(1);
 
-            /// WriteLine(students.GetType());
-            /// <summary>
-            /// TheroToHeroCRUDInVSCode.NajotTalimDbContext+EntityFrameworkQueryableExtensions`1[TheroToHeroCRUDInVSCode.NajotTalimDbContext+Student]
-            /// </summary>
-
-
-            foreach (var student in students)
-            {       
-                WriteLine(student.FullName);
-            }
+            WriteLine(student.FullName); 
         }
+
+        #pragma warning restore CS0169 // The field 'Program.studentRepository' is never used
     }
 }

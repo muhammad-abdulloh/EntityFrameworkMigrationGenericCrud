ac02e748b4ef765727948e2dd5d31f2618bfcd9d
Clever Coder <92303840+clevercoderr@users.noreply.github.com>
2022-02-27 15:55:11 +0500

    finished

--- Program.cs
+++ Program.cs
@@ -13,15 +13,21 @@ namespace TheroToHeroCRUDInVSCode
             ISudentRepository sudentRepository = new StudentRepository();
             // var student = await sudentRepository.GetAsync(x => x.Id == 1);
             // WriteLine(student.FullName);
-            var student = await sudentRepository.UpdateAsync(new Student 
-            { 
-                Id = 1, 
-                FullName = "Shavkatbek Jo'rayev", // Auto commit qiladigon *.bat fayl yasadim
-            }); 
+
+            // var student = await sudentRepository.UpdateAsync(new Student 
+            // { 
+            //     Id = 1, 
+            //     FullName = "Shavkatbek Jo'rayev", // Auto commit qiladigon *.bat fayl yasadim
+            // }); 
             
-            WriteLine(student.FullName);
+            // WriteLine(student.FullName);
+
+            // bool student = await sudentRepository.DeleteAsync(x => x.Id == 2); //Id si 2 ga teng bo'gan studentni o'chirvordik
+            // WriteLine(student); 
+                
         }
 
         #pragma warning restore // The field 'Program.studentRepository' is never used
     }
 }
+
